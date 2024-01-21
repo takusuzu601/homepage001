40ff71cdacecd4132f3674bde66a1beb366ac010
takusuzu601 <takusuzu601@gmail.com>
2024-01-21 16:30:10 +0900

    メディアクエリー設置

--- resources/views/pro001/index.blade.php
+++ resources/views/pro001/index.blade.php
@@ -4,19 +4,41 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Laravel</title>
     <!-- reset.css destyle -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
+
+    <!-- vite仕様 -->
+    @vite(['resources/css/pro001/app.css', 'resources/js/pro001/app.js'])
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
 </head>
 
 <body class="antialiased">
-    <div>
-        Hello
-    </div>
+    <header>
+        <h1>サンプルページ</h1>
+        <nav>
+            <ul>
+                <li>ホーム</li>
+                <li>PAGE01</li>
+                <li>PAGE02</li>
+                <li>PAGE03</li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section id="section01">
+            <h2>セクション01の見出し</h2>
+            <p>ここにセクションのコンテンツが表示されます。</p>
+        </section>
+    </main>
+
+    <footer id="footer">
+        <p>&copy; 2023 サンプルページ</p>
+    </footer>
+
 </body>
 
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/pro001/app.css",
+                "resources/js/pro001/app.js",
+            ],
             refresh: true,
         }),
     ],
